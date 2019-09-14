1a7ac271f595dd907ba81eafd0e1f9fdb0867796
janderlvf <janderlvf@gmail.com>
2019-09-14 10:21:19 -0300

    Payment, one-to-one association aula 03-16

--- src/main/java/br/edu/iftm/course/config/TestConfig.java
+++ src/main/java/br/edu/iftm/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.edu.iftm.course.entities.Category;
 import br.edu.iftm.course.entities.Order;
 import br.edu.iftm.course.entities.OrderItem;
+import br.edu.iftm.course.entities.Payment;
 import br.edu.iftm.course.entities.Product;
 import br.edu.iftm.course.entities.User;
 import br.edu.iftm.course.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
