2bc100f15471a80b7a4407c78368635c0766c2ad
janderlvf <janderlvf@gmail.com>
2019-09-21 14:51:18 -0300

    Product update

--- src/main/java/br/edu/iftm/course/resourses/ProductResourse.java
+++ src/main/java/br/edu/iftm/course/resourses/ProductResourse.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.iftm.course.dto.ProductCategoriesDTO;
 import br.edu.iftm.course.dto.ProductDTO;
+import br.edu.iftm.course.dto.UserDTO;
 import br.edu.iftm.course.services.ProductService;
 
 @RestController
@@ -48,6 +50,12 @@ public class ProductResourse {
 				.buildAndExpand(newDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDto);
 		
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto){
+		
+		ProductDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 	
 	
--- src/main/java/br/edu/iftm/course/services/ProductService.java
+++ src/main/java/br/edu/iftm/course/services/ProductService.java
@@ -1,21 +1,23 @@
 	package br.edu.iftm.course.services;
 	
 	import java.util.List;
-	import java.util.Optional;
-	import java.util.stream.Collectors;
-	
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.iftm.course.dto.CategoryDTO;
-	import br.edu.iftm.course.dto.ProductCategoriesDTO;
-	import br.edu.iftm.course.dto.ProductDTO;
-	import br.edu.iftm.course.entities.Category;
-	import br.edu.iftm.course.entities.Product;
-	import br.edu.iftm.course.repositories.CategoryRepository;
-	import br.edu.iftm.course.repositories.ProductRepository;
-	import br.edu.iftm.course.services.exceptions.ResourceNotFoundException;
+import br.edu.iftm.course.dto.ProductCategoriesDTO;
+import br.edu.iftm.course.dto.ProductDTO;
+import br.edu.iftm.course.entities.Category;
+import br.edu.iftm.course.entities.Product;
+import br.edu.iftm.course.repositories.CategoryRepository;
+import br.edu.iftm.course.repositories.ProductRepository;
+import br.edu.iftm.course.services.exceptions.ResourceNotFoundException;
 	
 	@Service
 	public class ProductService {
@@ -50,8 +52,33 @@ import br.edu.iftm.course.dto.CategoryDTO;
 	}
 	
 	
-	private void SetProductCategories(Product entity, List<CategoryDTO> categories) {
 	
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductCategoriesDTO dto) {		
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity,dto);
+			entity =  repository.save(entity);
+			return new ProductDTO(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, ProductCategoriesDTO dto) {
+
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		if(dto.getCategories() != null && dto.getCategories().size() > 0) {
+			SetProductCategories(entity, dto.getCategories());
+		}
+	
+	}
+	private void SetProductCategories(Product entity, List<CategoryDTO> categories) {
+		
 		entity.getCategories().clear();
 		for(CategoryDTO dto : categories) {
 			
