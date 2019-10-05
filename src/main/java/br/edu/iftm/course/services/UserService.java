e247ca0913fcbe74aa848a413a5efb7fb43c941a
janderlvf <janderlvf@gmail.com>
2019-10-05 13:38:34 -0300

    Authorization filter

--- src/main/java/br/edu/iftm/course/services/UserService.java
+++ src/main/java/br/edu/iftm/course/services/UserService.java
@@ -25,21 +25,21 @@ import br.edu.iftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<UserDTO> findAll(){
+
+	public List<UserDTO> findAll() {
 		List<User> list = repository.findAll();
 		return list.stream().map(e -> new UserDTO(e)).collect(Collectors.toList());
-		
+
 	}
-	
+
 	public UserDTO findById(Long id) {
-		
+
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new UserDTO(entity);
@@ -48,52 +48,48 @@ public class UserService implements UserDetailsService {
 	public UserDTO insert(UserInsertDTO dto) {
 		User entity = dto.toEntity();
 		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
-		 entity = repository.save(entity);
-		 return new UserDTO(entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
 	}
-	
-	public void delete(Long id) {		
-		try {		
-		repository.deleteById(id);
-		} catch (EmptyResultDataAccessException e) {			
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {	
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-}
+	}
 
 	@Transactional
-public UserDTO update(Long id, UserDTO dto) {		
-	try {
-		User entity = repository.getOne(id);
-		updateData(entity,dto);
-		entity =  repository.save(entity);
-		return new UserDTO(entity);
-	}catch (EntityNotFoundException e) {
-		throw new ResourceNotFoundException(id);
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-}
 
-private void updateData(User entity, UserDTO dto) {
+	private void updateData(User entity, UserDTO dto) {
 
-	entity.setName(dto.getName());
-	entity.setEmail(dto.getEmail());
-	entity.setPhone(dto.getPhone());
-	
-	
-	
-	
-	
-}
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setPhone(dto.getPhone());
 
-@Override
-public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	User user = repository.findByEmail(username);
-	if (user == null) {
-		throw new UsernameNotFoundException(username);
 	}
-	return user;
-	
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return user;
+
 	}
-	
+
 }
