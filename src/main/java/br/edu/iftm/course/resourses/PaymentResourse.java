8440acbc6ae0557712a4dc37b51075b181eeb382
janderlvf <janderlvf@gmail.com>
2019-10-16 18:11:35 -0300

    Payment PUT POST endpoints

--- src/main/java/br/edu/iftm/course/resourses/PaymentResourse.java
+++ src/main/java/br/edu/iftm/course/resourses/PaymentResourse.java
@@ -1,15 +1,22 @@
 package br.edu.iftm.course.resourses;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.edu.iftm.course.dto.PaymentDTO;
 import br.edu.iftm.course.dto.PaymentDTO;
 import br.edu.iftm.course.services.PaymentService;
 
@@ -33,6 +40,24 @@ public class PaymentResourse {
 	public ResponseEntity<PaymentDTO> findById(@PathVariable Long id){
 		
 		PaymentDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping
+	public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO dto){		
+		PaymentDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+		
+	}
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PaymentDTO> update(@PathVariable Long id, @RequestBody PaymentDTO dto){
+		
+		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
--- src/main/java/br/edu/iftm/course/services/PaymentService.java
+++ src/main/java/br/edu/iftm/course/services/PaymentService.java
@@ -4,34 +4,73 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.edu.iftm.course.dto.CategoryDTO;
 import br.edu.iftm.course.dto.PaymentDTO;
+import br.edu.iftm.course.entities.Category;
+import br.edu.iftm.course.entities.Order;
 import br.edu.iftm.course.entities.Payment;
+import br.edu.iftm.course.repositories.OrderRepository;
 import br.edu.iftm.course.repositories.PaymentRepository;
 import br.edu.iftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class PaymentService {
-	
+
 	@Autowired
 	private PaymentRepository repository;
 	
-	
-public List<PaymentDTO> findAll(){
-		
+	@Autowired
+	private OrderRepository orderRepository;
+
+
+	public List<PaymentDTO> findAll() {
+
 		List<Payment> list = repository.findAll();
 		return list.stream().map(e -> new PaymentDTO(e)).collect(Collectors.toList());
-		
+
 	}
-	
+
 	public PaymentDTO findById(Long id) {
 
 		Optional<Payment> obj = repository.findById(id);
 		Payment entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new PaymentDTO(entity);
+
+	}
+	
+	@Transactional
+	public PaymentDTO insert(PaymentDTO dto) {
+	Order order = orderRepository.getOne(dto.getOrderId());
+	Payment payment = new Payment(null, dto.getMoment(), order);
+	order.setPayment(payment);
+	orderRepository.save(order);
+	return new PaymentDTO(order.getPayment());
+
+	}
+
+	@Transactional
+	public PaymentDTO update(Long id, PaymentDTO dto) {
+		try {
+			Payment entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new PaymentDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Payment entity, PaymentDTO dto) {
+
+		entity.setMoment(dto.getMoment());
 		
 	}
+
 
 }
